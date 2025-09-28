61d45199c4d4af89d75ebc890dcdfa86b542f19a
Son_Tran_207 <sontran200797@gmail.com>
2025-09-28 19:11:03 +0700

    Setup_Character_Class

---
+++ Assets/Scripts/CharacterData/StaticDataCharacter.cs
@@ -0,0 +1,16 @@
+
+public class StaticDataCharacter
+{
+    public float Health { get; set; }
+
+    public float Attack { get; set; }
+
+    public float Magic { get; set; }
+
+    public float AttackDefense { get; set; }
+
+    public float MagicDefense { get; set; }
+
+    public float Stamina { get; set; }
+
+}
---
+++ Assets/Scripts/PlayerData/StaticDataPlayer.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class StaticDataPlayer
+{
+    private int PlayerID { get; set; }
+    private string PlayerName { get; set; }
+    private string CharacterClass { get; set; }
+    private int PlayerLevel { get; set; }
+    private float ExperiencePoints { get; set; }
+
+}
