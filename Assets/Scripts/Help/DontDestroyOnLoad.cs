3c20ad24faf298097d7f8ceec46a4a68fa81edc9
Son_Tran_207 <sontran200797@gmail.com>
2025-10-13 19:34:12 +0700

    Set_up_Manager

---
+++ Assets/Scripts/Help/DontDestroyOnLoad.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class DontDestroyOnLoad : MonoBehaviour
+{
+    private static GameObject _instance;
+
+    void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this.gameObject;
+            DontDestroyOnLoad(this);
+            
+        }
+        else if (_instance != gameObject)
+        {
+            Debug.Log("Object bị hủy vì đã có instance tồn tại - " + gameObject.name);
+            Destroy(gameObject);
+        }
+    }
+}
