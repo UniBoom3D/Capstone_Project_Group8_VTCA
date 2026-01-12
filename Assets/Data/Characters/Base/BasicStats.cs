45acb6dec1abc80964c6c356eb9337445b94745f
Son_Tran_207 <sontran200797@gmail.com>
2026-01-12 15:40:50 +0700

    12/1/26

--- Assets/Data/Characters/Base/BasicStats.cs
+++ Assets/Data/Characters/Base/BasicStats.cs
@@ -9,4 +9,14 @@ public struct BasicStats
     public int _magic;
     public int _armor;
     public int _magicResist;
+
+    public BasicStats(int health, int stamina, int attack, int magic, int armor, int magicResist)
+    {
+        _health = health;
+        _stamina = stamina;
+        _attack = attack;
+        _magic = magic;
+        _armor = armor;
+        _magicResist = magicResist;
+    }
 }
