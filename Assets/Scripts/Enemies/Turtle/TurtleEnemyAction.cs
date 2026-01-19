440d35dfad7b8ce606d2dd507224e267e2e81de4
quangthuong123 <125529148+quangthuong123@users.noreply.github.com>
2026-01-19 11:01:08 +0700

    shooting
    fixed PVE script, move shooting and complete the shooting logic

--- Assets/Scripts/Enemies/Turtle/TurtleEnemyAction.cs
+++ Assets/Scripts/Enemies/Turtle/TurtleEnemyAction.cs
@@ -1,20 +1,17 @@
 using UnityEngine;
 
-
-[CreateAssetMenu(fileName = "TurtleEnemyData", menuName = "Enemy/Static/Turtle")]
-public class TurtleEnemyAction : StaticDataEnemies
+public class TurtleEnemyAction : MonoBehaviour // <--- Now it can be attached!
 {
-
+    // Drag your "TurtleData" asset here in the Inspector
+    public StaticDataEnemies enemyData;
 
     void Update()
     {
-
+        // Example: Access data through the variable
+        // float currentSpeed = enemyData.moveSpeed; 
     }
 
     public void MoveToPlayer() { }
-    public void AttackPlayer()
-    { }
-    public void OnDestroy() 
-    { }
-
+    public void AttackPlayer() { }
+    public void OnDestroy() { }
 }
