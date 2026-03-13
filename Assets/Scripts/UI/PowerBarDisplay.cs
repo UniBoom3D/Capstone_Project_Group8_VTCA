32a9936a2e94106016a48e9a01e12ef83c2282dc
Son_Tran_207 <sontran200797@gmail.com>
2026-03-13 20:26:44 +0700

    Setup Power Bar

--- Assets/Scripts/UI/PowerBarDisplay.cs
+++ Assets/Scripts/UI/PowerBarDisplay.cs
@@ -1,36 +1,34 @@
 ﻿using UnityEngine;
-using UnityEngine.UI; // Bắt buộc phải có để thao tác với UI (Slider, Image)
+using UnityEngine.UI;
 
-[RequireComponent(typeof(Slider))]
 public class PowerBarDisplay : MonoBehaviour
 {
     private Slider powerSlider;
 
-    [Tooltip("Kéo thả GameObject 'Fill' nằm bên trong Slider vào đây")]
-    public Image fillImage;
-
-    [Tooltip("Thiết lập dải màu. Bên trái (0) là Xanh lá, bên phải (1) là Đỏ")]
-    public Gradient colorGradient;
+    [Header("Settings")]
+    public Image maskFillImage;
 
     void Start()
     {
-        // Lấy component Slider mà script này đang gắn vào
         powerSlider = GetComponent<Slider>();
 
-        // Cập nhật màu sắc ngay lúc bắt đầu game
-        UpdateColor(powerSlider.value);
-
-        // Lắng nghe sự kiện: Mỗi khi value của Slider thay đổi, gọi hàm UpdateColor
-        powerSlider.onValueChanged.AddListener(UpdateColor);
+        
     }
 
-    // Hàm cập nhật màu sắc dựa trên giá trị của Slider
-    void UpdateColor(float value)
+    private void LateUpdate()
     {
-        if (fillImage != null)
+        UpdateMask(powerSlider.value);
+
+        powerSlider.onValueChanged.AddListener(UpdateMask);
+    }
+    void UpdateMask(float value)
+    {
+        if (maskFillImage != null)
         {
-            // normalizedValue tự động đưa giá trị Slider về khoảng 0 - 1 (phù hợp với Gradient)
-            fillImage.color = colorGradient.Evaluate(powerSlider.normalizedValue);
+            // Đảo ngược logic: 
+            // Slider 0 (Hết máu) -> FillAmount 1 (Che hết)
+            // Slider 1 (Đầy máu) -> FillAmount 0 (Lộ hết)
+            maskFillImage.fillAmount = 1f - powerSlider.normalizedValue;
         }
     }
 }
